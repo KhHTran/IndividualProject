2ed215888e1e2d420220c65ba86c1989ccb867b8
iGrimJack <khoahieu.tran@outlook.com>
2019-02-07 14:06:58 +0000

    Now call instance of IPMemory on the blockchain

--- MemberManager.sol
+++ MemberManager.sol
@@ -2,57 +2,63 @@ pragma solidity ^0.4.24;
 
 import "./IPMemory.sol";
 
-contract MemberManagement {
+contract MemberManager {
+	IPMemory mem;
+	address public owner;
+	bytes32 constant primaryHash = keccak256(abi.encodePacked("Primary"));
+	bytes32 constant secondaryHash = keccak256(abi.encodePacked("Secondary"));
+	event MemberRegistered(address _member, string _type);
+
+	modifier onlyOwner() {
+		require(msg.sender == owner,"Can only performed by owner");
+		_;
+	}
+
 	modifier memberNotRegistered(address _member,string _type) {
 		bytes32 crypt = encrypt(_member,_type);
-		require(IPMemory.getUint(crypt) == 0,"member is already registered");
+		require(mem.getUint(crypt) == 0,"member is already registered");
 		_;
 	}
 
 	modifier memberRegistered(address _member,string _type) {
 		bytes32 crypt = encrypt(_member,_type);
-		require(IPMemory.getUint(crypt) > 0,"member is not registered");
+		require(mem.getUint(crypt) > 0,"member is not registered");
 		_;
 	}
 
-	modifier memberIsPrime() {
-		bytes32 crypt = encrypt(msg.sender,"Primary");
-		require(IPMemory.getUint(crypt) > 0 || msg.sender == owner,"not a primary member");
-		_;
+	function MemberManager(address _mem) public {
+		mem = IPMemory(_mem);
+		owner = msg.sender;
 	}
 
 	function encrypt(address _address, string _string) internal returns(bytes32) {
-		return keccak256(abi.encodePacked(_address,_string))
+		return keccak256(abi.encodePacked(_address,_string));
 	}
 
 	function registerMember(address _member, string _type, string _metadata) 
 	external 
 	memberNotRegistered(_member,_type) {
-		require(_type == "Primary" || _type == "Secondary","Member type should be Primary or Secondary");
+	    bytes32 hash = keccak256(abi.encodePacked(_type));
+		require(hash == primaryHash || hash == secondaryHash,"Member type should be Primary or Secondary");
 		require(bytes(_metadata).length > 0, "Data needed to be provided");
 		bytes32 crypt = encrypt(_member,_type);
-		IPMemory.storeUint(crypt,1);
+		mem.storeUint(crypt,1);
 		crypt = keccak256(abi.encodePacked(_member,_type,"Metadata"));
-		IPMemory.storeString(crypt,_metadata);
+		mem.storeString(crypt,_metadata);
 		emit MemberRegistered(_member,_type);
 	}
 
 	function getMemberData(address _member, string _type) external
 	memberRegistered(_member,_type) returns(string) {
 		bytes32 crypt = keccak256(abi.encodePacked(_member,_type,"Metadata"));
-		return IPMemory.getString(crypt);
+		return mem.getString(crypt);
 	}
 
 	function updataMemberData(address _member, string _type, string _metadata) 
 	external 
 	memberRegistered(_member,_type) {
 		require(bytes(_metadata).length > 0, "Data needed to be provided");
-		crypt = keccak256(abi.encodePacked(_member,_type,"Metadata"));
-		IPMemory.storeString(crypt,_metadata);
-	}
-
-	function registerEvent(string _name, string _url, uint _start, uint _end, uint _ticketPrice, bytes _signature) 
-	external memberIsPrime(msg.sender){
-
+		bytes32 crypt = keccak256(abi.encodePacked(_member,_type,"Metadata"));
+		mem.storeString(crypt,_metadata);
 	}
 }
