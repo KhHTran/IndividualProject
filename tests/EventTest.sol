4b46fc86693b84a9776a4c10863058d798329321
Hieu Tran <khoahieu.tran@outlook.com>
2019-03-14 13:16:09 +0000

    Event Test for event manager

--- tests/EventTest.sol
+++ tests/EventTest.sol
@@ -1,24 +1,23 @@
 pragma solidity ^0.4.24;
 
 import "./IPMemory.sol";
-import "./EventManager.sol";
+import "./EventLib.sol";
 import "remix_tests.sol";
 
 contract EventTest {
 	address owner = msg.sender;
 	IPMemory mem;
-	EventManager eventMan;
 	address userP = 0x14723a09acff6d2a60dcdf7aa4aff308fddc160c;
 	address userS = 0x4b0897b0513fdc7c541b6d9d7e929c4e5364d2db;
 	uint eventID;
 
 	modifier onlyOwner() {
 		require(msg.sender == owner, "Not authorise to do action");
+		_;
 	}
 
-	function setContract(address _mem, address _event) external onlyOwner() {
+	function setContract(address _mem) external onlyOwner() {
 		mem = IPMemory(_mem);
-		eventMan = EventManager(_event);
 		bytes32 crypt = keccak256(abi.encodePacked(userP,"Primary"));
 		mem.storeUint(crypt,1);
 		crypt = keccak256(abi.encodePacked(userS,"Secondary"));
@@ -26,45 +25,45 @@ contract EventTest {
 	}
 
 	function testBadParamsCreateEvent() external {
-		// Call by address of userP
 		ThrowProxy throwproxy = new ThrowProxy(address(eventMan)); 
-	    EventManager(address(throwproxy)).registerEvent("","",now + 1, now + 10, uint(8));
-	    bool r = throwproxy.execute.gas(200000)(); 
-	    Assert.equal(false, r, "Empty name/URL revert");
-	   
-	    EventManager(address(throwproxy)).registerEvent("EventTest","example.com",now + 10, now + 1, uint(10));
-	    r = throwproxy.execute.gas(200000)();
-	    Assert.equal(r,false,"Illegal time revert");
-
-	    EventManager(address(throwproxy)).registerEvent("","",now + 1, now + 10, uint(0));
-	    r = throwproxy.execute.gas(200000)();
-	    Assert.equal(r,false,"Non-positive price revert");
+		EventManager(address(throwproxy)).registerEvent(userP,"","",now + 1, now + 10, uint(8));
+		bool r = throwproxy.execute.gas(200000)(); 
+		Assert.equal(false, r, "Empty name/URL revert");
+		EventManager(address(throwproxy)).registerEvent(userP,"EventTest","example.com",now + 10, now + 1, uint(10));
+		r = throwproxy.execute.gas(200000)();
+		Assert.equal(r,false,"Illegal time revert");
+    	EventManager(address(throwproxy)).registerEvent(userP,"","",now + 1, now + 10, uint(0));
+    	r = throwproxy.execute.gas(200000)();
+    	Assert.equal(r,false,"Non-positive price revert");
 	}
 
 	function testRepeateEventCreate() external {
-	    EventManager(address(throwproxy)).registerEvent("EventTest","example.com",start,end,10);
+	    EventManager(address(throwproxy)).registerEvent(userP,"EventTest","example.com",start,end,10);
 	    r = throwproxy.execute.gas(200000)();
 	    Assert.equal(r,false,"Event hash actived revert");
 	}
 
-	function testCreateEvent() external {
-		if (msg.sender == userP) {
-			eventID = eventMan.getEventCount();
-		    uint start = now;
-		    start += 300;
-		    uint end = start + 120;
-		    eventMan.registerEvent("EventTest","example.com",start,end,10);
-			bytes32 hash = keccak256(abi.encodePacked("EventTest","example.com",start,end,10));
-			bytes32 _key = keccak256(abi.encodePacked(eventID,"Event Hash"));
-			Assert.equal(eventMan.getEventCount(), eventID + 1,"event count test");
-			Assert.equal(mem.getBytes32(_key),hash,"storing event hash test");
-		}
-		else {
-			ThrowProxy throwproxy = new ThrowProxy(address(eventMan)); 
-	    	EventManager(address(throwproxy)).registerEvent();
-	    	bool r = throwproxy.execute.gas(200000)(); 
-	    	Assert.equal(false, r, "Secondary user cannot create event");
-		}
+	function testCreateEventPrime() external {
+		bytes32 _keyCount = keccak256(abi.encodePacked("Event Library Event Number Count"));
+		eventID = mem.getUint(_keyCount);
+		uint start = now;
+		start += 300;
+		uint end = start + 120;
+		eventMan.registerEvent(userP,"EventTest","example.com",start,end,10);
+		bytes32 hash = keccak256(abi.encodePacked("EventTest","example.com",start,end,10));
+		bytes32 _key = keccak256(abi.encodePacked(eventID,"Event Hash"));
+		Assert.equal(mem.getUint(_keyCount), eventID + 1,"event count test");
+		Assert.equal(mem.getBytes32(_key),hash,"storing event hash test");
+	}
+
+	function testCreateEventSecondary() external {
+		uint start = now;
+		start += 300;
+		uint end = start + 120;
+		ThrowProxy throwproxy = new ThrowProxy(address(eventMan)); 
+		EventManager(address(throwproxy)).registerEvent(userS,"Event test 2","Example.com2",start,end,uint(10));
+		bool r = throwproxy.execute.gas(200000)();
+		Assert.equal(false, r, "Secondary user cannot create event");
 	}
 
 	function testEventData() external {
@@ -75,13 +74,12 @@ contract EventTest {
 	}
 
 	function testCancelEvent() external {
-		memMan.cancelEvent(eventID);
+		memMan.cancelEvent(userP,eventID);
 		_key = keccak256(abi.encodePacked(hash,"Event Active Status"));
 		Assert.equal(0,mem.getUint(_key),"Event Cancel test");
 	}
 }
 
-// Proxy contract for testing throws
 contract ThrowProxy {
 	address public target;
 	bytes data;
